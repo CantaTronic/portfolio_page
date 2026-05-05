8f10466e0180a77e0e86c8897dccc3c934a3fcff
Victoria Tokareva <victoria.tokareva@kit.edu>
2026-05-05 17:02:55 +0200

    fix tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: 'https://cantatronic.github.io',
   base: '/portfolio_page',
 });
